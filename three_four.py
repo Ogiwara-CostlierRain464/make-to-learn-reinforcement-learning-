654f58abd4fb59470f85f380bfab05b81fc0a1d6
ogiwara <yushiogiwara@gmail.com>
2019-10-08 22:48:22 +0900

    copy sample

--- three_four.py
+++ three_four.py
@@ -56,7 +56,7 @@ class Agent:
 
     def get_action(self, observation, step):
         """行動の決定"""
-        action = self.brain.deceide_action(observation, step)
+        action = self.brain.decide_action(observation, step)
         return action
 
 
@@ -104,3 +104,71 @@ class Brain:
             action = np.argmax(self.q_table[state][:])
         return action
 
+
+class Environment:
+    def __init__(self):
+        self.env = gym.make(ENV)
+        # 課題の状態の数4を取得
+        num_states = self.env.observation_space.shape[0]
+        # CartPoleの行動の2を取得
+        num_actions = self.env.action_space.n
+
+        self.agent = Agent(num_states, num_actions)
+
+    def run(self):
+        # 195step以上連続でたち続けた試行数
+        complete_episodes = 0
+        is_episode_final = False
+        frames = []
+
+        for episode in range(NUM_EPISODES):
+            observation = self.env.reset()
+
+            for step in range(MAX_STEPS):
+                if is_episode_final is True:
+                    frames.append(self.env.render(mode="rgb_array"))
+
+                # 行動を求める
+                action = self.agent.get_action(observation, episode)
+
+                # 行動a_tの実行により、s_{t+1}, r_{t+1}を求める
+                observation_next, _, done, _ = self.env.step(action)
+
+                # 報酬を与える
+                # ステップ数が200経過するか、一定角度以上傾くとdoneはtrueになる
+                if done:
+                    if step < 195:
+                        # 途中でこけたら罰則として報酬-1を与える
+                        # 195step以上連続でたち続けた試行数をリセット
+                        reward = -1
+                        complete_episodes = 0
+
+                    else:
+                        # 立ったまま終了時は報酬1を与える
+                        # 連続記録を更新
+                        reward = 1
+                        complete_episodes += 1
+                else:
+                    # 途中の報酬は0
+                    reward = 0
+
+                # step+1の状態observation_nextを用いて、Q関数を更新する
+                self.agent.update_Q_function(observation, action, reward, observation_next)
+
+                observation = observation_next
+
+                if done:
+                    print("{0} Episode: Finished after {1} time steps".format(episode, step + 1))
+                    break
+
+            if is_episode_final is True:
+                display_frames_as_git(frames)
+                break
+
+            if complete_episodes >= 10:
+                print("10回連続成功")
+                is_episode_final = True
+
+
+cartpole_env = Environment()
+cartpole_env.run()
